4227a5672603447e65a28f6438bc14b9413b97ba
Inclemental-55 <riverb514@gmail.com>
2021-03-07 18:39:51 -0700

    Final Touches

--- main.py
+++ main.py
@@ -5,29 +5,25 @@ import os
 import random
 
 pygame.font.init()
-WIN_WIDTH = 500
-WIN_HEIGHT = 800
+WIDTH = 500
+HEIGHT = 800
 
-BIRD_IMGS = [pygame.transform.scale2x(pygame.image.load(os.path.join("imgs", "bird1.png"))),
-             pygame.transform.scale2x(pygame.image.load(os.path.join("imgs", "bird2.png"))),
-             pygame.transform.scale2x(pygame.image.load(os.path.join("imgs", "bird1.png")))]
-PIPE_IMG = pygame.transform.scale2x(pygame.image.load(os.path.join("imgs", "pipe.png")))
-BASE_IMG = pygame.transform.scale2x(pygame.image.load(os.path.join("imgs", "base.png")))
-BG_IMG = pygame.transform.scale2x(pygame.image.load(os.path.join("imgs", "bg.png")))
+BIRDS = [pygame.transform.scale2x(pygame.image.load(os.path.join("imgs", "bird1.png"))),
+         pygame.transform.scale2x(pygame.image.load(os.path.join("imgs", "bird2.png"))),
+         pygame.transform.scale2x(pygame.image.load(os.path.join("imgs", "bird1.png")))]
+PIPES = pygame.transform.scale2x(pygame.image.load(os.path.join("imgs", "pipe.png")))
+BASE_IMAGE = pygame.transform.scale2x(pygame.image.load(os.path.join("imgs", "base.png")))
+BACKGROUND = pygame.transform.scale2x(pygame.image.load(os.path.join("imgs", "bg.png")))
 
-FONT = pygame.font.SysFont("comicsans", 50)
+font = pygame.font.SysFont("comicsans", 50)
 
 generation = 0
 
-# WIN = pygame.display.set_mode((WIN_WIDTH, WIN_HEIGHT))
-# pygame.display.set_caption("Flappy Bird")
-
-
 class Bird:
-    IMGS = BIRD_IMGS
-    MAX_ROTATION = 25
-    ROT_VEL = 20
-    ANIMATION_TIME = 5
+    IMAGES = BIRDS
+    MAX_ROT = 25
+    VEL = 20
+    TICK_TIME = 5
 
     def __init__(self, x, y):
         self.x = x
@@ -37,7 +33,7 @@ class Bird:
         self.vel = 0
         self.height = self.y
         self.img_count = 0
-        self.img = self.IMGS[0]
+        self.img = self.IMAGES[0]
 
     def jump(self):
         self.vel = -10.5
@@ -55,29 +51,29 @@ class Bird:
         self.y += d
 
         if d < 0 or self.y < self.height + 50:
-            if self.tilt < self.MAX_ROTATION:
-                self.tilt = self.MAX_ROTATION
+            if self.tilt < self.MAX_ROT:
+                self.tilt = self.MAX_ROT
         else:
             if self.tilt > -90:
-                self.tilt -= self.ROT_VEL
+                self.tilt -= self.VEL
 
     def draw(self, win):
         self.img_count += 1
 
-        if self.img_count < self.ANIMATION_TIME:
-            self.img = self.IMGS[0]
-        elif self.img_count < self.ANIMATION_TIME * 2:
-            self.img = self.IMGS[1]
-        elif self.img_count < self.ANIMATION_TIME * 3:
-            self.img = self.IMGS[2]
-        elif self.img_count < self.ANIMATION_TIME * 4:
-            self.img = self.IMGS[1]
-        elif self.img_count < self.ANIMATION_TIME * 4 + 1:
-            self.img = self.IMGS[0]
+        if self.img_count < self.TICK_TIME:
+            self.img = self.IMAGES[0]
+        elif self.img_count < self.TICK_TIME * 2:
+            self.img = self.IMAGES[1]
+        elif self.img_count < self.TICK_TIME * 3:
+            self.img = self.IMAGES[2]
+        elif self.img_count < self.TICK_TIME * 4:
+            self.img = self.IMAGES[1]
+        elif self.img_count < self.TICK_TIME * 4 + 1:
+            self.img = self.IMAGES[0]
             self.img_count = 0
         if self.tilt <= -80:
-            self.img = self.IMGS[1]
-            self.img_count = self.ANIMATION_TIME * 2
+            self.img = self.IMAGES[1]
+            self.img_count = self.TICK_TIME * 2
         rotated_img = pygame.transform.rotate(self.img, self.tilt)
         rect = rotated_img.get_rect(center=self.img.get_rect(topleft=(self.x, self.y)).center)
         win.blit(rotated_img, rect.topleft)
@@ -95,8 +91,8 @@ class Pipe:
         self.height = 0
         self.top = 0
         self.bottom = 0
-        self.PIPE_TOP = pygame.transform.flip(PIPE_IMG, False, True)
-        self.PIPE_BOTTOM = PIPE_IMG
+        self.PIPE_TOP = pygame.transform.flip(PIPES, False, True)
+        self.PIPE_BOTTOM = PIPES
 
         self.passed = False
         self.set_height()
@@ -130,8 +126,8 @@ class Pipe:
 
 class Base:
     VEL = 5
-    WIDTH = BASE_IMG.get_width()
-    IMG = BASE_IMG
+    WIDTH = BASE_IMAGE.get_width()
+    IMG = BASE_IMAGE
 
     def __init__(self, y):
         self.y = y
@@ -153,13 +149,13 @@ class Base:
 
 
 def draw_window(win, birds, pipes, base, score, gen):
-    win.blit(BG_IMG, (0, 0))
+    win.blit(BACKGROUND, (0, 0))
     for pipe in pipes:
         pipe.draw(win)
-    text = FONT.render("Score: " + str(score), True, (255, 255, 255))
-    win.blit(text, (WIN_WIDTH - 10 - text.get_width(), 10))
+    text = font.render("Score: " + str(score), True, (255, 255, 255))
+    win.blit(text, (WIDTH - 10 - text.get_width(), 10))
 
-    text = FONT.render("Gen: " + str(gen), True, (255, 255, 255))
+    text = font.render("Gen: " + str(gen), True, (255, 255, 255))
     win.blit(text, (10, 10))
     base.draw(win)
     for bird in birds:
@@ -183,7 +179,7 @@ def main(genomes, config):
     score = 0
     global generation
     generation += 1
-    win = pygame.display.set_mode((WIN_WIDTH, WIN_HEIGHT))
+    win = pygame.display.set_mode((WIDTH, HEIGHT))
     clock = pygame.time.Clock()
     run_bool = True
 
